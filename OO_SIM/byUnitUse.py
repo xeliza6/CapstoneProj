9e783d380a25fcdd906d1323f8fae672e39146e2
hokiedude12 <robmaj12@vt.edu>
2018-03-18 17:13:09 -0400

    Pushing the byUnitUsage Plots

---
+++ OO_SIM/byUnitUse.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 18 16:39:12 2018
+
+@author: robmaj12
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 11:10:39 2018
+
+@author: robmaj12
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import oo_simulation
+#run the simulation
+system_state_record, transfer_record = oo_simulation.main()
+units=['A','B','C','D','E','F','G','H','I','J']
+#fig, axs = plt.subplots(10,1,sharex=True)
+#fig.subplots_adjust(hspace=0)
+
+index = 1
+#Make dataframe to conatin the system_state_record
+for unit in units:
+    plt.figure(index)
+    df = pd.DataFrame(columns=['time', 'online', 'maintenance','offline', 'asset_demand', 'shortage'])
+    i = 0
+    while i < len(system_state_record[unit]):
+        df.loc[i] = system_state_record[unit][i]
+        i = i+1
+    
+
+    online= list(df['online'])
+    maintenance= list(df['maintenance'])
+    offline= list(df['offline'])
+    demand = list(df['asset_demand'])
+    shortage = list(df['shortage'])
+    time= list(df['time'])
+    plt.style.use('fivethirtyeight')
+    plt.title('Unit '+unit+' Usage Plot')
+    plt.xlabel('Time in Days',fontsize=12)
+    plt.ylabel('Number of Assets',fontsize=12)
+    plt.stackplot(time,online, maintenance, offline, labels=['Online','Maintenance','Offline'])
+    index=index+1
+    plt.legend(loc='lower left')
+plt.show()
--- OO_SIM/systemUse.py
+++ OO_SIM/systemUse.py
@@ -6,8 +6,6 @@ Created on Tue Feb 13 11:10:39 2018
 @author: robmaj12
 """
 import pandas as pd
-import plotly as py
-import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 
 import oo_simulation
@@ -21,35 +19,6 @@ while i < len(system_state_record['global']):
     df.loc[i] = system_state_record['global'][i]
     i = i+1
     
-#j = 0
-#while j < len(df['time']):
-#    df['maintenance'].loc[j] = df['online'][j]+df['maintenance'][j]
-#    df['offline'].loc[j] = df['online'][j]+df['offline'][j]
-#    j = j + 1
-    
-
-##set up lines to plot    
-#online = go.Scatter(x=df['time'],y=df['online'],fill='lines',name='Online')
-#maint = go.Scatter(x=df['time'],y=df['maintenance'], fill ='lines',name='Maintenance')
-#offline = go.Scatter(x=df['time'],y=df['offline'],fill='lines',name='Offline')
-#demand = go.Scatter(x=df['time'],y=df['asset_demand'], fill ='tonexty',name='Demand')
-#shortage =go.Scatter(x=df['time'],y=df['shortage'],fill = 'lines',name='Shortage')
-##orgainze the data and create a a layout
-#data= [shortage,offline,maint,online]
-#layout = go.Layout(dict(title = 'Total Asset System Usage'),
-#    showlegend=True,
-#    xaxis=dict(
-#        type='linear', title= 'Time in Days',
-#    ),
-#    yaxis=dict(
-#        type='linear',title='Number of Assets',
-#        dtick=10
-#    )
-#)
-#    #create figure and plot
-#fig = go.Figure(data=data,layout=layout)
-#
-#py.offline.plot(fig, filename ='systemUse.html')
 online= list(df['online'])
 maintenance= list(df['maintenance'])
 offline= list(df['offline'])
@@ -60,5 +29,5 @@ plt.style.use('fivethirtyeight')
 plt.title('System Usage Plot')
 plt.xlabel('Time in Days',fontsize=12)
 plt.ylabel('Number of Assets',fontsize=12)
-plt.stackplot(time,online, maintenance, offline,shortage, labels=['Online','Maintenance','Offline','Demand Shortage'])
-plt.legend(loc='upper right')
+plt.stackplot(time,online, maintenance, offline, labels=['Online','Maintenance','Offline'])
+plt.legend(loc='lower left')
